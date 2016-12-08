38528a0014bc5095fb1c1883618288a18317e9fe
Ji Huang <cocafehj@qq.com>
2016-12-09 06:28:47 +0800

    sched: enable feature arch_power and disable gentle_fair_sleepers

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -3,7 +3,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Place new tasks ahead so that they do not starve already running
@@ -42,9 +42,13 @@ SCHED_FEAT(CACHE_HOT_BUDDY, true)
 /*
  * Use arch dependent cpu power functions
  */
-SCHED_FEAT(ARCH_POWER, false)
+SCHED_FEAT(ARCH_POWER, true)
 
+/*
+ * Without SCHED_HRTICK this feature has no effect
+ */
 SCHED_FEAT(HRTICK, false)
+
 SCHED_FEAT(DOUBLE_TICK, false)
 SCHED_FEAT(LB_BIAS, true)
 
