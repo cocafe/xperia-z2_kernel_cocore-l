105efeb209ed58862289665b94fb7677619dbc60
Ji Huang <cocafehj@qq.com>
2017-04-06 23:00:53 +0800

    sched: enable next_buddy feature
    Signed-off-by: Ji Huang <cocafehj@qq.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -24,7 +24,7 @@ SCHED_FEAT(AFFINE_WAKEUPS, true)
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, false)
+SCHED_FEAT(NEXT_BUDDY, true)
 
 /*
  * Prefer to schedule the task that ran last (when we did
